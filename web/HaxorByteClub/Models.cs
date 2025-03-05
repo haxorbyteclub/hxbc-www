4a8b6e3389693186dbc7f5084dc4e1c5befa7184
JL <nor0x@users.noreply.github.com>
2025-03-05 14:40:08 +0100

    audioplayer fixes

--- web/HaxorByteClub/Models.cs
+++ web/HaxorByteClub/Models.cs
@@ -2,7 +2,7 @@ namespace HaxorByteClub;
 
 public record Track(string Id, string Name, string Artist, string Duration, string Genre, string Url)
 {
-	public string[] Genres => Name.Split('_').Select(g => g.Replace('-', ' ')).ToArray();
+	public string[] Genres => Genre.Split('_').Select(g => g.Replace('-', ' ')).ToArray();
 
 	public int DurationInSeconds => int.Parse(Duration.Split(':')[0]) * 60 + int.Parse(Duration.Split(':')[1]);
 }
