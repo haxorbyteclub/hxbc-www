673cfc80a79d1cef3739599dcc2fea3534613deb
JL <nor0x@users.noreply.github.com>
2025-03-21 20:35:19 +0100

    more #16 and #18
    also noticed that #6 is already working

--- web/HaxorByteClub/Models/Message.cs
+++ web/HaxorByteClub/Models/Message.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Mvvm.Messaging.Messages;
 namespace HaxorByteClub.Models;
 
 public class Message
@@ -5,4 +6,24 @@ public class Message
 	public string Text { get; set; }
 	public string User { get; set; }
 	public DateTime Date { get; set; }
+}
+
+
+public class ShowEditorMessage : ValueChangedMessage<EditorLaunchSettings>
+{
+	public ShowEditorMessage(EditorLaunchSettings value) : base(value)
+	{
+	}
+}
+
+public class EditorLaunchSettings
+{
+	public string? Text { get; set; }
+	public string? Title { get; set; }
+
+	public EditorLaunchSettings(string? text, string? title)
+	{
+		Text = text;
+		Title = title;
+	}
 }
