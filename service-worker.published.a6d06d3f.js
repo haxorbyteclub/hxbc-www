1f74d4898b805f6398721a2403c81fa4197fba85
nor0x <nor0x@users.noreply.github.com>
2025-08-07 06:52:09 +0000

    Deploying to gh-pages from @ haxorbyteclub/hxbc-www@54392d5fda9996017a9964ee79232a6728de0c8c 🚀

--- service-worker.published.a6d06d3f.js
+++ service-worker.published.a6d06d3f.js
@@ -1,4 +1,3 @@
-/* Manifest version: xAhLEtx3 */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
